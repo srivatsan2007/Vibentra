39506f1a0ad92bd79a2faf13284332aaae3e3800
srivatsan2007 <srivatsanr9h13@gmail.com>
2026-06-21 11:11:22 +0530

    Delete youtube files

--- backend/routes/youtubeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const youtubeController = require('../controllers/youtubeController');
-
-router.get('/search', youtubeController.search);
-router.get('/song', youtubeController.getSongDetails);
-
-module.exports = router;
